716460fd1e5fb4e999bbc5cb325337025b4c6ed7
mrhl082024 <mhrl082024@gmail.com>
2024-09-05 12:39:46 +0200

    first

---
+++ Program.cs
@@ -0,0 +1,44 @@
+class Movie
+{
+    public string Title { get; set; }
+
+    public Movie(string title)
+    {
+        Title = title;
+    }
+}
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        // Register the list with the Dependency Injection Containter
+        builder.Services.AddSingleton<List<Movie>>();
+        var app = builder.Build();
+
+        // READ: Get all movies
+        app.MapGet("/movies", (List<Movie> movies) => movies);
+        //CREATE: Adds a new movie
+        app.MapPost("/movies", (Movie? movie, List<Movie> movies) =>
+        {
+            if (movie == null)
+            {
+                return Results.BadRequest();
+            }
+            movies.Add(movie);
+
+            return Results.Created();
+        });
+
+        // DELETE: Delete a movie with id
+        app.MapDelete("/movies/{id}", (int Id) => $"Delete movie with id: {Id}");
+        // UPDATE: Update a movie with id
+        app.MapPut("/movies/{id}", (int Id) => $"Update movie with id: {Id}");
+
+        app.MapGet("/health", () => "System healthy");
+
+        app.Run();
+    }
+}
